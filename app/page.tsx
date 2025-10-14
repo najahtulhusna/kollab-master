1f374e7e71387f7071805c1474216f16cdac4ab9
SyauqiSuhaimi <97452632+SyauqiSuhaimi@users.noreply.github.com>
2025-10-14 14:59:01 +0800

    Migrate from Mantine to Shadcn UI and Tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { Switch } from "@mantine/core";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <div>
+    <div className="h-screen w-screen flex flex-col items-center justify-center gap-4">
       <h1 className="text-3xl font-bold underline">Test Tailwind</h1>
-      <Switch defaultChecked label="Test Mantine" />
+
       <h4>Test supabase in /login page</h4>
+      <Button>Test Shadcn</Button>
     </div>
   );
 }
